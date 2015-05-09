314004dbca661e74643c951d0f1b4a218f0cfd24
Taruni Kancharla <tkanchar@asu.edu>
2015-05-09 13:53:35 -0700

    Homework 6 Files

---
+++ homework6/src/main/java/edu/asu/bmi/cda/homework6/App.java
@@ -0,0 +1,13 @@
+package edu.asu.bmi.cda.homework6;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
